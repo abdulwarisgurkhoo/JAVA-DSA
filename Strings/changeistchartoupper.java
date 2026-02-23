f12876ca7ae144a92ad57a6feed491ccbb69abbf
Abdul Waris Gurkhoo <gurkhooabdulwaris@gmail.com>
2026-02-23 07:57:02 +0530

    chagetoupper

---
+++ Strings/changeistchartoupper.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class changeistchartoupper {
+   
+    /**
+     * @param str
+     */
+    public static String changetoupper(String str)
+    {
+        StringBuilder sb=new StringBuilder();
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        for(int i=1;i<str.length();i++)
+        {
+            if(str.charAt(i)==' ' && i<str.length()-1)
+            {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else
+            {
+                 sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+    public static void main(String[] args) {
+        String str="hi, im abdul waris ";
+        String st1=changetoupper(str);
+        System.out.println(st1);
+    }
+}
